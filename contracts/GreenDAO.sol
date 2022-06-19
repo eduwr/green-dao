fceefd46d365cb02de905d470416c30ffa9699d9
eduwr <eduwric@gmail.com>
2022-06-19 19:33:59 -0300

    feat: :sparkles: create GreenDAO ERC721 token

---
+++ contracts/GreenDAO.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract GreenDAO is ERC721 {
+    string[] public names;
+    mapping(string => bool) public nameExists;
+
+
+    modifier uniqueName(string memory _name) {
+        require(!nameExists[_name], "Names must be unique"); 
+        _;
+    }
+
+    constructor() ERC721("GreenDAO", "GREEN") {}
+
+    function mint(string memory _name) public uniqueName(_name) {
+        nameExists[_name] = true;
+        names.push(_name);
+        uint newItemId = names.length - 1;
+        _safeMint(msg.sender, newItemId);
+    }
+}
