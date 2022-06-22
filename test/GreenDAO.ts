7822179d0d8c15907ed62e524336c1e47267b882
eduwr <eduwric@gmail.com>
2022-06-21 23:12:18 -0300

    test: Signer should not have balance when contract is deployed

--- test/GreenDAO.ts
+++ test/GreenDAO.ts
@@ -2,10 +2,11 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 // eslint-disable-next-line node/no-missing-import
 import { GreenDAO } from "../typechain";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-describe("GreenDAO", function () {
+describe("GreenDAO", () => {
   let greenDAO: GreenDAO;
-
+  let sender: SignerWithAddress;
   beforeEach(async () => {
     const GreenDAO = await ethers.getContractFactory("GreenDAO");
     greenDAO = await GreenDAO.deploy();
@@ -17,11 +18,19 @@ describe("GreenDAO", function () {
     expect(await greenDAO.symbol()).to.equal("GREEN");
   });
 
+  it("Signer should not have balance when contract is deployed", async () => {
+    [sender] = await ethers.getSigners();
+    const ownerTokens = await greenDAO.balanceOf(sender.address);
+    expect(ownerTokens).to.equal(0);
+  });
+
   it("Should be able to mint a token", async () => {
-    const [sender] = await ethers.getSigners();
+    [sender] = await ethers.getSigners();
     await greenDAO.mint("Eduardo", { from: sender.address });
 
     const owner = await greenDAO.ownerOf("0");
+    const ownerTokens = await greenDAO.balanceOf(sender.address);
+    expect(ownerTokens).to.equal(1);
     expect(owner).to.equal(sender.address);
   });
 });
