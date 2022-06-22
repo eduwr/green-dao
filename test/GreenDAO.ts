07dcd217b562515f196c8b2b55ef80b9993e591e
eduwr <eduwric@gmail.com>
2022-06-21 22:54:32 -0300

    test: test mint function

--- test/GreenDAO.ts
+++ test/GreenDAO.ts
@@ -1,13 +1,30 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+// eslint-disable-next-line node/no-missing-import
+import { GreenDAO } from "../typechain";
 
 describe("GreenDAO", function () {
-  it("Should deploy the contract correctly", async () => {
-    const GreenDAO = await ethers.getContractFactory("GreenDAO");
-    const greenDAO = await GreenDAO.deploy();
-    await greenDAO.deployed();
+  let greenDAO: GreenDAO;
 
+  beforeEach(async () => {
+    const GreenDAO = await ethers.getContractFactory("GreenDAO");
+    greenDAO = await GreenDAO.deploy();
+    await greenDAO.deployed();
+  });
+
+  it("Should deploy the contract correctly", async () => {
     expect(await greenDAO.name()).to.equal("GreenDAO");
     expect(await greenDAO.symbol()).to.equal("GREEN");
+  });
+
+  it("Should be able to mint a token", async () => {
+    const [sender] = await ethers.getSigners();
+
+    console.log("balance", await sender.getBalance());
+
+    await greenDAO.mint("Eduardo", { from: sender.address });
+
+    const owner = await greenDAO.ownerOf("0");
+    expect(owner).to.equal(sender.address);
   });
 });
