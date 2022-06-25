c356ec1c26b683f5f08e9a780c6e7e14edd0a87a
eduwr <eduwric@gmail.com>
2022-06-25 20:31:26 -0300

    feat: add vote capabilities to the contract

--- contracts/GreenDAO.sol
+++ contracts/GreenDAO.sol
@@ -1,10 +1,11 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 import "hardhat/console.sol";
-
 
 contract GreenDAO is ERC721 {
     string[] public names;
@@ -18,7 +19,6 @@ contract GreenDAO is ERC721 {
 
     constructor() ERC721("GreenDAO", "GREEN") {
       console.log("Deploying a GreenDAO with `GREEN` symbol.");
-
     }
 
     function mint(string memory _name) public uniqueName(_name) {
@@ -26,5 +26,14 @@ contract GreenDAO is ERC721 {
         names.push(_name);
         uint newItemId = names.length - 1;
         _safeMint(msg.sender, newItemId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Votes)
+    {
+        super._afterTokenTransfer(from, to, tokenId);
     }
 }
